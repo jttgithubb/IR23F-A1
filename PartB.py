8d54a2493a0f03f84ccb046a024c3b8f42c37f4b
Jonathan Tran <tranjt7@uci.edu>
2023-10-13 04:56:23 -0700

    added time complexity analysis for PartB and finished the assignment

--- PartB.py
+++ PartB.py
@@ -2,6 +2,13 @@ import sys
 from PartA import tokenize, computeWordFrequencies
 
 
+''' intersection Time Complexity: O(N^2)
+    The intersection function below runs the computeWordFrequencies and tokenize functions once for each input file
+    which is O(N) and O(N^2) time respectively. Afterwards, we iterate through the tokens of the the first input file
+    and check if the second input contains the same tokens, incrementing along the way which runs in O(N) because of the for loop
+    and counting and dictionary contains being constant operations. As a result, the time complexity can be described as 
+    O(N^2 + N + N^2 + N + N) or more simply put O(N^2) because it dominates.
+'''
 def intersection(textFile1, textFile2):
     intersectCount = 0
     file1Dict = computeWordFrequencies(tokenize(textFile1))
