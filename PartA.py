80025fe4e4069f0176b130f245aad17192584e99
Jonathan Tran <tranjt7@uci.edu>
2023-10-13 03:36:49 -0700

    added comments analyzing the time complexity of each function in partA

--- PartA.py
+++ PartA.py
@@ -5,6 +5,12 @@ import sys
 originalPrint = print 
 
 
+''' tokenize Time Complexity: O(N^2)
+    The tokenize function below contains a main loop which iterates through each line of the file, two inner loops that process 
+    the current line to either remove invalid characters or access the words in that line, and two string functions that process the 
+    current line in O(n) time. Because the function only contains one main loop and the nested loops and functions that process the 
+    current line, the time complexity of this function can be described as O(N(N + N + N + N)) or more simply put O(N^2).
+'''
 def tokenize(textFilePath):
     allTokensList = []
     with open(textFilePath, 'r', encoding = 'utf-8') as file:
@@ -20,6 +26,11 @@ def tokenize(textFilePath):
     return allTokensList
 
 
+''' computeWordFrequencies Time Complexity: O(N)
+    The computeWordFrequencies function below only comprises of a single loop that iterates through the list of tokens argument.
+    Within the loop, tokens are added to the dictionary if not yet seen and their count value is incremented in constant time. Thus, 
+    the time complexity of this function can be described as O(N(1 + 1)) or more simply put O(N).
+'''
 def computeWordFrequencies(tokenList):
     wordFreqDict = {}
     for token in tokenList:
@@ -29,6 +40,11 @@ def computeWordFrequencies(tokenList):
     return wordFreqDict
 
 
+''' print Time Complexity: O(NlogN)
+    The print function below comprises of two instances of sorting through the sorted function to create a descending frequency list 
+    that is also alphabetical in order. Additionally, the final list is iterated to print the results to the console. Therefore, the time
+    complexity of this function can be described as O(NlogN + NlogN + N) which can be simplified to O(NlogN). 
+'''
 def print(wordFrequencyDict):
     for w, f in sorted(sorted(wordFrequencyDict.items()), key = lambda entry:  entry[1], reverse = True):
         originalPrint(w + "\t" + str(f))
